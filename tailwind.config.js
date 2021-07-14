a93457899c0688e8b1b44d8dd68691455c543186
Ben Nagle-Rose <bnaglerose@vpt.co.uk>
2021-07-14 19:39:54 +0100

    add tailwind to create-react-app

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// Fairly basic tailwind config that doesn't make any changes to the defaults
+// It does however include a plugin for forms which helps with styling elements like selects
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
